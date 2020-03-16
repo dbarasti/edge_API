c4b3fedceda3d66fd882b89ea2f68bb24c137416
Davide Barasti <dbarasti@gmail.com>
2020-03-16 18:51:00 +0100

    added schema for bumps

---
+++ app_api/models/bump-data.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const pointSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['Point'],
+    required: true
+  },
+  coordinates: {
+    type: [Number],
+    required: true
+  }
+});
+
+const sensorDataSchema = new mongoose.Schema({
+  timestamp: Number,
+  x: Number,
+  y: Number,
+  z: Number,
+  location: {
+    type: pointSchema,
+    required: true
+  }
+})
+
+const bumpDataSchema = new mongoose.Schema({
+  bumpID: String,
+  sensorData: [sensorDataSchema]
+});
+
+var BumpModel = mongoose.model("BumpData", bumpDataSchema, "bumps");
+
+module.exports = BumpModel
