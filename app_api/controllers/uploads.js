f1645dd1c7ffd6e93ebba8d33833cad4b3cb9e5b
Davide Barasti <dbarasti@gmail.com>
2020-03-24 18:44:13 +0100

    fixed code for bump data upload

--- app_api/controllers/uploads.js
+++ app_api/controllers/uploads.js
@@ -17,7 +17,7 @@ app.use(bodyParser.raw({type: 'application/json'}))
 var Storage = multer.diskStorage({
   destination: (req, file, callback) => {
     // the file.fieldname MUST be a unique id
-    const dir = `./Frames/${file.fieldname}`
+    const dir = `./Frames`
     mkdirp.sync(dir) // creates the directory, if not already present
     callback(null, dir)
   },
@@ -48,8 +48,8 @@ const uploadsGetForm = (req, res) => {
 
 const uploadsSaveBumpData = (req,res) => {
   req.body.data.forEach(bumpEvent => {
-    let bumpCoordinates = bumpEvent.attached_sensors_data.forEach(sensorData=>{
-      let bumpData = [];
+    let bumpData = [];
+    bumpEvent.attached_sensors_data.forEach(sensorData=>{
       bumpData.push(new models.SensorDataModel({
         timestamp: sensorData.timestamp,
         x: sensorData.x,
@@ -67,7 +67,7 @@ const uploadsSaveBumpData = (req,res) => {
         type: 'Point',
         coordinates: [bumpEvent.latitude,bumpEvent.longitude]
       },
-      sensorData: bumpCoordinates
+      sensorData: bumpData
     });
 
     bump.save((err) => {
