a1870f96beda0456fb45fe85e318cc6cafda00c2
Davide Barasti <dbarasti@gmail.com>
2020-05-05 20:03:26 +0200

    removed useless imports

--- app_api/controllers/cloud.js
+++ app_api/controllers/cloud.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
 const util = require('util')
 
 
